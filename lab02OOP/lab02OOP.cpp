86829e95acbce02c47de773e92a295c9f8cc8005
kammarx <kammarufa6@gmail.com>
2021-12-05 01:00:00 +0500

    first commit

--- lab02OOP/lab02OOP.cpp
+++ lab02OOP/lab02OOP.cpp
@@ -1,8 +1,31 @@
-
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
+
+
+class Point {
+public:
+	int x, y;
+	Point() {
+		cout << "Point()\n";
+		x = 0;
+		y = 0;
+	}
+	Point(int x, int y) {
+		cout << "Point(int x, int y)\n";
+		this->x = x;
+		this->y = y;
+	}
+
+	Point(const Point &p) {
+		cout << "Point(const  Point &p)\n";
+		x = p.x;
+		y = p.y;
+	}
+
+};
 int main()
 {
 	cout << "haha";
