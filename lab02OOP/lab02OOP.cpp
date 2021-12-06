af307a02396f7172c37826e1473b3dcccdfaf05f
kammarx <kammarufa6@gmail.com>
2021-12-06 21:21:17 +0500

    some changes

--- lab02OOP/lab02OOP.cpp
+++ lab02OOP/lab02OOP.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 
 class Point {
-public:
+protected:
 	int x, y;
+public:
 	Point() {
 		cout << "Point()\n";
 		x = 0;
@@ -25,11 +26,34 @@ public:
 		y = p.y;
 	}
 
+	~Point() {
+		cout << x << " " << y << endl;;
+		cout << "~Point()\n";
+	}
+
+	void move(int dx, int dy) {
+		x = x + dx;
+		y = y = dy;
+	}
+
 };
+
+
 int main()
 {
-	cout << "haha";
+	
+		Point p1;
+		Point p2(50, 80);
+		Point p3(p2);
 
 
+		Point* p4 = new Point;
+		Point* p5 = new Point(23, 34);
+		Point* p6 = new Point(*p5);
 
+		delete p4;
+		delete p5;
+		delete p6;
+
+		return 0;
 }
